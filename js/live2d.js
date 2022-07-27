4bc62c0406f8930ce17b62e638b5a5ad620608a2
Larthur <2403048893@qq.com>
2022-07-27 13:20:24 +0800

    Site updated: 2022-07-27 13:20:23

---
+++ js/live2d.js
@@ -0,0 +1,25 @@
+addEventListener("DOMContentLoaded", function () {
+    let models = [
+      {
+        left: "0px",
+        bottom: "0px",
+        basePath:
+          "https://fastly.jsdelivr.net/gh/alg-wiki/AzurLaneL2DViewer@gh-pages/assets",
+        role: "bisimai_2",
+        background: "",
+        opacity: 1,
+        mobile: false,
+      },
+      {
+        right: "0px",
+        bottom: "0px",
+        basePath:
+          "https://fastly.jsdelivr.net/gh/alg-wiki/AzurLaneL2DViewer@gh-pages/assets",
+        role: "bisimai_2",
+        background: "",
+        opacity: 1,
+        mobile: false,
+      },
+    ];
+    new Live2dLoader(models);
+  });
